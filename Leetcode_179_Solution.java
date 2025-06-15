a97a6779aebe60300c309e1f57254cb2c664c49e
rishiparashar02 <rishivparashar@gmail.com>
2025-06-15 14:15:34 +0530

    solution-added

---
+++ Leetcode_179_Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        String[] strNums = new String[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            strNums[i] = String.valueOf(nums[i]);
+        }
+
+        Arrays.sort(strNums, (a, b) -> (b + a).compareTo(a + b));
+
+        if (strNums[0].equals("0")) return "0";
+
+        StringBuilder result = new StringBuilder();
+        for (String s : strNums) {
+            result.append(s);
+        }
+
+        return result.toString();
+    }
+}
